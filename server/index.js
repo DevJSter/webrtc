b4d6b833bfd3dff7c759e0d4bdebe4f3e8001ae0
Shubham Tiwari <shubht3303@gmail.com>
2024-05-04 19:57:55 +0530

    backend

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173"
+    origin: ["https://webrtc-red.vercel.app"],
+    methods:["POST","GET"],
+    credintials: true
+
   }
 });
 
